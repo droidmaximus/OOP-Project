32a6f4fc84c7f416a835c94974ab46262f896803
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2021-11-06 07:35:06 +0530

    Adding insert

--- OOPSproject/src/Main.java
+++ OOPSproject/src/Main.java
@@ -3,40 +3,40 @@ package OOPSproject.src;
 import java.sql.*;
 
 public class Main {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/OOP_project";  // Database URL
+    static final String DB_URL = "jdbc:mysql://localhost:3306/OOP_project"; // Database URL
     static final String USER = "root"; // Database Username
-    static final String PASS = "Mohit2001";    // Database Password
+    static final String PASS = "password"; // Database Password
 
     public static void main(String[] args) {
-        switch (args[0]) {  // flags 
+        switch ("-p") { // flags
 
-        case "-h":  // help
-        { 
+        case "-h": // help
+        {
             System.out.println(" -h: help");
             System.out.println(" -c: create table ");
             System.out.println(" -p: print all the columns");
             break;
         }
 
-        case "-c":    //create
+        case "-c": // create
         {
             Operations_implementation op1 = new Operations_implementation();
             try {
                 Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
                 Statement stmt = con.createStatement();
                 String sql;
-                String [] columnName=new String[4];
-                String [] columnType=new String[4];
-                columnName[0]="id";
-                columnName[1]="first";
-                columnName[2]="last";
-                columnName[3]="age";
-                columnType[0]="INTEGER";
-                columnType[1]="VARCHAR(255)";
-                columnType[2]="VARCHAR(255)";
-                columnType[3]="INTEGER"; 
+                String[] columnName = new String[4];
+                String[] columnType = new String[4];
+                columnName[0] = "id";
+                columnName[1] = "first";
+                columnName[2] = "last";
+                columnName[3] = "age";
+                columnType[0] = "INTEGER";
+                columnType[1] = "VARCHAR(255)";
+                columnType[2] = "VARCHAR(255)";
+                columnType[3] = "INTEGER";
 
-                sql=op1.createTable("REGISTRATION", columnName, columnType);
+                sql = op1.createTable("REGISTRATION", columnName, columnType);
 
                 stmt.executeUpdate(sql);
                 System.out.println("Table created successfully");
@@ -47,18 +47,37 @@ public class Main {
             break;
         }
 
-        case "-p":
-        {
+        case "-p": {
             Operations_implementation op1 = new Operations_implementation();
             try {
                 Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
                 Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery(op1.printTable("REGISTRATION"));
                 while (rs.next()) {
-                    System.out.println("ID = " + rs.getInt(1) + ", First = " + rs.getString(2) + ", Last = " + rs.getString(3) + ", Age = " + rs.getInt(4));
+                    System.out.println("ID = " + rs.getInt(1) + ", First = " + rs.getString(2) + ", Last = "
+                            + rs.getString(3) + ", Age = " + rs.getInt(4));
                 }
                 rs.close();
                 con.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            break;
+        }
+
+        case "-i": {
+            try {
+                Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = con.createStatement();
+                String sql = "INSERT INTO Registration VALUES (100, 'Zara', 'Ali', 18)";
+                stmt.executeUpdate(sql);
+                sql = "INSERT INTO Registration VALUES (101, 'Mahnaz', 'Fatma', 25)";
+                stmt.executeUpdate(sql);
+                sql = "INSERT INTO Registration VALUES (102, 'Zaid', 'Khan', 30)";
+                stmt.executeUpdate(sql);
+                sql = "INSERT INTO Registration VALUES(103, 'Sumit', 'Mittal', 28)";
+                stmt.executeUpdate(sql);
+                System.out.println("Inserted records into the table...");
             } catch (Exception e) {
                 System.out.println(e);
             }
